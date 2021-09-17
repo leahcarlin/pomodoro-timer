90d25893644938d8262e357d11443e8c291890cf
leahcarlin <leahrcarlin@gmail.com>
2021-09-17 15:11:01 +0200

    Initialize timer andadd msg if click while timer running

--- index.js
+++ index.js
@@ -3,21 +3,31 @@ const timerEl = document.getElementById("time-display");
 const workBtn = document.getElementById("work-btn");
 const shortBreakBtn = document.getElementById("short-break-btn");
 const longBreakBtn = document.getElementById("long-break-btn");
+const messageEl = document.getElementById("message-display");
 
 const work = 25;
 const shortBreak = 5;
 const longBreak = 15;
 
+function displayZeroTime() {
+  timerEl.innerHTML = `0:00`
+}
+
 function updateTimer(time) {
   let start = setInterval(function() {
     const minutes = Math.floor(time / 60);
     let seconds = time % 60;
-
-    if (seconds < 10) {
-      seconds = "0" + seconds;
-      } else {
-        seconds = seconds;
-      }
+    if (time > 0) {
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+        } else {
+          seconds = seconds;
+        }
+    } else {
+      displayZeroTime();
+      clearInterval(start);
+      countdown = false;
+    }
 
     timerEl.innerHTML = `${minutes}:${seconds}`;
     time--;
@@ -26,20 +36,37 @@ function updateTimer(time) {
 
 workBtn.addEventListener("click", function(){
   console.log("Work button clicked");
-  updateTimer(25*60);
-  infoEl.innerHTML = "Time to Work!"
+  if (countdown === false){
+    countdown = true;
+    updateTimer(25*60);
+    infoEl.innerHTML = "Time to Work!"
+  } else {
+    messageEl.innerHTML = "Countdown already in progress"
+  }
 })
 
 shortBreakBtn.addEventListener("click", function(){
   console.log("Short break button clicked");
-  updateTimer(5*60);
-  infoEl.innerHTML = "Time for a short break!"
+  if (countdown === false){
+    countdown = true;
+    updateTimer(5*60);
+    infoEl.innerHTML = "Time for a short break!"
+  } else {
+    messageEl.innerHTML = "Countdown already in progress"
+  }
 })
 
 longBreakBtn.addEventListener("click", function(){
   console.log("Long break button clicked");
-  updateTimer(15*60);
-  infoEl.innerHTML = "Time for a long break!"
+  if (countdown === false){
+    countdown = true;
+    updateTimer(15*60);
+    infoEl.innerHTML = "Time for a long break!"
+  } else {
+    messageEl.innerHTML = "Countdown already in progress"
+  }
 })
 
-
+//initilalize timer
+displayZeroTime();
+let countdown = false;
